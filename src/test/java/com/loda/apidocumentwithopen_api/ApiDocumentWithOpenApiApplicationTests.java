e12abd89e53980ab97b0e14a885a4e02e8ef68ca
TxmMinh <93864928+TxmMinh@users.noreply.github.com>
2023-03-06 01:36:19 +0700

    API Document with OPEN API 3.0

---
+++ src/test/java/com/loda/apidocumentwithopen_api/ApiDocumentWithOpenApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loda.apidocumentwithopen_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiDocumentWithOpenApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
